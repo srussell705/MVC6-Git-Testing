81c4ae86f188576216952d62f7bcbfba9f0a3591
Stephen Russell <stephen.russell@ringcontainer.com>
2020-07-01 17:20:56 -0500

    Push of controlers and views

---
+++ MVC6 Git Testing/Controllers/SecurityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC6_Git_Testing.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC6 Git Testing/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC6_Git_Testing.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryID = new SelectList(db.tbl_Category, "CategoryID", "CategoryName");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(tbl_Url objURL)
+        {
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryID = new SelectList(db.tbl_Category, "CategoryID", "CategoryName");
+            return View();
+        }
+
+    }
+}
